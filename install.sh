ea040286c2455323d70d2079127e95953bac17ee
Mikołaj Data <miki3867@gmail.com>
2021-11-28 14:05:04 +0100

    Rename scripts

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 cd /tmp
-curl https://raw.githubusercontent.com/yungtry/papiezmowi/master/papiezmowi > papiezmowi
+curl https://raw.githubusercontent.com/miki164/papiezmowi/master/papiezmowi.py > papiezmowi
 chmod +x papiezmowi
 printf '\n\nPotrzebuje uprawnien zeby móc zainstalować papiezmowi\n'
 sudo rm -f /usr/local/bin/papiezmowi
 sudo rm -f /usr/local/bin/papiez-papa
 sudo mv papiezmowi /usr/local/bin
-curl https://raw.githubusercontent.com/yungtry/papiezmowi/master/papiez-papa > papiez-papa
+curl https://raw.githubusercontent.com/yungtry/papiezmowi/master/papiez-papa.sh > papiez-papa
 chmod +x papiez-papa
 sudo mv papiez-papa /usr/local/bin
 echo '
@@ -33,4 +33,4 @@ echo '
 ░░█░░░░░░░▀▄▄▄▒▒▒▒▒▒▄▀░░░░█░░░░░░
 ░░░█░░░░░░░░░▌▀▀▀▀▀▀▐░░░░░▐▌░░░░░
 '
-printf 'Pomyślnie zainstalowano papiezmowi. Użycie komendy znajdziesz na https://github.com/yungtry/papiezmowi. Aby odinstalować skrypt wpisz w terminalu: papiez-papa\n'
+printf 'Pomyślnie zainstalowano papiezmowi. Użycie komendy znajdziesz na https://github.com/miki164/papiezmowi. Aby odinstalować skrypt wpisz w terminalu: papiez-papa\n\nOryginalne repo -> https://github.com/yungtry/papiezmowi'
